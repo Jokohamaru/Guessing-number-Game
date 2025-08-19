db3609c079b91913f96d18069d88756dc94dc6cf
Kien Nguyen <kiennguyen3112@gmail.com>
2025-08-19 21:08:03 +0700

    update

--- guessing.py
+++ guessing.py
@@ -1,34 +1,43 @@
 import random_num
-import math
 
 def playing(lifepoint, num):
     target_num = random_num.random_number(num)
     print(target_num)
     while lifepoint > 0:
-        user_input = int(input("ENTER YOUR NUMBER: "))
+        while(True):
+            user_input = int(input("ENTER YOUR NUMBER: "))
+            if (user_input<=num):
+                break
+            else:
+                print(f"You're out of range, plz enter number below or equal with {num} \n")
         if check(target_num,user_input):
-            print("You're right, excellent!!!\n")
             return
         else :
             lifepoint -= 1
     
     print(("Your lifepoints are over").upper())
-            
-       
+             
 def check(target, guess):
     if guess == target:
-        print("You're correct!!")
+        print("You're right, excellent!!!\n")
         return 1
     else:
+        isGreater(guess,target)
         if abs(target - guess) < 3:
-            print("Almost there!!")
+            print("Almost there!!\n")
             return 0
         elif abs(target - guess) < 5:
-            print("So close.")
+            print("So close.\n")
             return 0
         elif abs(target - guess) < 20:
-            print("nearly!!!")
+            print("nearly!!!\n")
             return 0
         else:
-            print("Wrong number")
+            print("Wrong number.\n")
             return 0
+        
+def isGreater (inp, tar):
+    if inp > tar:
+        print(f"Is lower than {inp}")
+    else:
+        print(f"Is higher than {inp}")
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     
     # Choosing difficult level
     while(True):
-        print("1. EASY (10 chances and choosing from 1 to 100)")
+        print("1. EASY (10 chances and choosing f3rom 1 to 100)")
         print("2. MEDIUM (5 chances and choosing from 1 to 100)")
         print("3. HARD (3 chances and choosing form 1 to 100)")
         print("4. HELL (20 chances and choosing from 1 to any number (<1000000)) (IN PROGRESS)")
@@ -21,6 +21,8 @@ def main():
                 guessing.playing(5,100)
             case 3:
                 guessing.playing(3,100)
+            case 4:
+                guessing.playing(20,1000000)
             case 5:
                 break
 
